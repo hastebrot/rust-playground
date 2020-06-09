cc6459fd41426329cbc5abc068fcf37b05419065
Benjamin Gudehus <hastebrot@gmail.com>
2020-06-10 00:26:26 +0200

    (feature) Add calculate_depth() to tree.rs.

--- hello-trees/examples/tree.rs
+++ hello-trees/examples/tree.rs
@@ -1,18 +1,29 @@
-fn main() {
-    let root = Node::new(1);
+use std::cmp::max;
 
-    println!("{:?}", root);
-    // Node { value: 1, left: None, right: None }
+fn main() {
+    let mut root = Node::new(1);
+    root.left = Some(Box::new(Node::new(2)));
+
+    println!("root = {:?}", root);
+    println!("depth = {}", calculate_depth(Some(Box::new(root)), 0));
+}
+
+fn calculate_depth<T>(edge: Edge<T>, depth: i32) -> i32 {
+    match edge {
+        None => depth,
+        Some(node) => max(
+            calculate_depth(node.left, depth + 1),
+            calculate_depth(node.right, depth + 1),
+        ),
+    }
 }
 
 #[derive(Debug)]
 struct Node<T> {
     value: T,
-    left: Link<T>,
-    right: Link<T>,
+    left: Edge<T>,
+    right: Edge<T>,
 }
-
-type Link<T> = Option<Box<Node<T>>>;
 
 impl<T> Node<T> {
     fn new(value: T) -> Self {
@@ -24,3 +35,4 @@ impl<T> Node<T> {
     }
 }
 
+type Edge<T> = Option<Box<Node<T>>>;
